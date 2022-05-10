9b30dbd7e36fa23b0e467dfd0cc8910f29dd9d84
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-05-10 09:52:46 +0200

    Add Set-CouchDBSession test

--- tests/Authentication.Tests.ps1
+++ tests/Authentication.Tests.ps1
@@ -2,3 +2,10 @@ BeforeAll {
     Import-Module ../PSCouchDB/PSCouchDB.psm1
     . ../PSCouchDB/functions/CouchDBauthentication.ps1
 }
+
+Describe "Set-CouchDBSession" {
+    It "Set cookie authentication." {
+        $password = "password" | ConvertTo-SecureString -AsPlainText -Force
+        Set-CouchDBSession -UserId admin -Password $password | Should -BeLike '*'
+    }
+}
