26e9606f61eddc184b5946b28fa3fabb829854ff
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-05-11 11:35:58 +0200

    Add New-CouchDBAdmin test

--- tests/Permission.Tests.ps1
+++ tests/Permission.Tests.ps1
@@ -2,3 +2,10 @@ BeforeAll {
     Import-Module ../PSCouchDB/PSCouchDB.psm1
     . ../PSCouchDB/functions/CouchDBpermission.ps1
 }
+
+Describe "New-CouchDBAdmin" {
+    It "Create a new admin user." {
+        $password = "password" | ConvertTo-SecureString -AsPlainText -Force
+        (New-CouchDBAdmin -Userid newadminuser -Password $password -Authorization "admin:password").newadminuser | Should -BeLike '*'
+    }
+}
