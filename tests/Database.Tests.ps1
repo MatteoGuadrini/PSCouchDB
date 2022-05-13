f25d08bd4df32760ae436dd087f3d9e44587a987
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-05-13 09:25:30 +0200

    Add Test-CouchDBDatabase test

--- tests/Database.Tests.ps1
+++ tests/Database.Tests.ps1
@@ -2,3 +2,9 @@ BeforeAll {
     Import-Module ../PSCouchDB/PSCouchDB.psm1
     . ../PSCouchDB/functions/CouchDBdatabase.ps1
 }
+
+Describe "Test-CouchDBDatabase" {
+    It "Test if database exists." {
+        Test-CouchDBDatabase -Database _users | Should -BeLike '*'
+    }
+}
