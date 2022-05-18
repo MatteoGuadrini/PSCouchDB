7c19a0749e63f2873c9a201e7e0eed663e79a155
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-05-18 11:27:05 +0200

    Add Get-CouchDBDocument test

--- tests/Document.Tests.ps1
+++ tests/Document.Tests.ps1
@@ -1,4 +1,11 @@
 BeforeAll {
     Import-Module ../PSCouchDB/PSCouchDB.psm1
     . ../PSCouchDB/functions/CouchDBdocument.ps1
+    New-CouchDBDatabase -Database test -Authorization "admin:password"
+}
+
+Describe "Get-CouchDBDocument" {
+    It "Get a document." {
+        (Get-CouchDBDocument -Database test -Document "Hitchhikers")._id | Should -Be "Hitchhikers"
+    }
 }
