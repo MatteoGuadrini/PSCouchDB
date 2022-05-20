0433bc8b025684d1d85c310f46e336bd0097f4fd
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-05-20 09:18:38 +0200

    Add Set-CouchDBDocument test

--- tests/Document.Tests.ps1
+++ tests/Document.Tests.ps1
@@ -4,16 +4,23 @@ BeforeAll {
     New-CouchDBDatabase -Database test -Authorization "admin:password"
 }
 
+Describe "New-CouchDBDocument" {
+    It "Create a new document." {
+        $data = @{"answer"=42; "ask"="Ultimate Question of Life, the Universe and Everything"}
+        (New-CouchDBDocument -Database test -Document "Hitchhikers" -Data $data -Partition Guide -Authorization "admin:password").ok | Should -Be 'true'
+    }
+}
+
 Describe "Get-CouchDBDocument" {
     It "Get a document." {
         (Get-CouchDBDocument -Database test -Document "Hitchhikers")._id | Should -Be "Hitchhikers"
     }
 }
 
-Describe "New-CouchDBDocument" {
-    It "Create a new document." {
-        $data = @{"answer"=42; "ask"="Ultimate Question of Life, the Universe and Everything"}
-        (New-CouchDBDocument -Database test -Document "Hitchhikers" -Data $data -Partition Guide -Authorization "admin:password").ok | Should -Be 'true'
+Describe "Set-CouchDBDocument" {
+    It "Modify a document." {
+        $data = @{"answer"=43; "ask"="Ultimate Question of Life, the Universe and Everything"}
+        (Set-CouchDBDocument -Database test -Document "Hitchhikers" -Revision (Get-CouchDBDocument -Database test -Document "Hitchhikers")._rev -Data $data -Authorization "admin:password").ok | Should -Be 'true'
     }
 }
 
