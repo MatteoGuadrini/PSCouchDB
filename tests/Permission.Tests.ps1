970c3455036039ee7ed1a2214acfbb19ab7702d4
Matteo Guadrini <matteo.guadrini@hotmail.it>
2022-06-03 10:47:54 +0200

    Fix permission cmdlet tests

--- tests/Permission.Tests.ps1
+++ tests/Permission.Tests.ps1
@@ -1,3 +1,4 @@
+using module ../PSCouchDB/PSCouchDB.psm1
 BeforeAll {
     Import-Module ../PSCouchDB/PSCouchDB.psm1
     . ../PSCouchDB/functions/CouchDBpermission.ps1
@@ -32,39 +33,38 @@ Describe "Remove-CouchDBAdmin" {
 Describe "New-CouchDBUser" {
     It "Create a new user." {
         $password = "password" | ConvertTo-SecureString -AsPlainText -Force
-        (New-CouchDBAdmin -Userid newadminuser -Password $password -Authorization "admin:password").ok | Should -Be "true"
+        (New-CouchDBUser -Userid test_user -Password $password -Authorization "admin:password").ok | Should -Be 'true'
     }
 }
 
 Describe "Set-CouchDBUser" {
     It "Set an user properties." {
         $password = "newpassword" | ConvertTo-SecureString -AsPlainText -Force
-        (Set-CouchDBUser -Userid test_user -Password $password -Revision (Get-CouchDBUser -Userid test_user -Authorization "admin:password").rev -Authorization "admin:password").ok | Should -Be "true"
+        (Set-CouchDBUser -Userid test_user -Password $password -Revision (Get-CouchDBUser -Userid test_user -Authorization "admin:password")._rev -Authorization "admin:password").ok | Should -Be 'true'
     }
 }
 
 Describe "Remove-CouchDBUser" {
     It "Remove an user." {
-        (Remove-CouchDBUser -Userid test_user -Revision (Get-CouchDBUser -Userid test_user -Authorization "admin:password").rev -Authorization "admin:password" -Force).ok | Should -Be "true"
+        (Remove-CouchDBUser -Userid test_user -Revision (Get-CouchDBUser -Userid test_user -Authorization "admin:password")._rev -Authorization "admin:password" -Force).ok | Should -Be 'true'
     }
 }
 
 Describe "Grant-CouchDBDatabasePermission" {
     It "Grant the security object for the given database." {
-        (New-CouchDBDatabase -Database test -Authorization "admin:password").ok | Should -Be "true"
         $sec = @"
-    {
-        "admins": {
-            "names": [],
-            "roles": []
-        },
-        "members": {
-            "names": ["reader"],
-            "roles": []
-        }
+{
+    "admins": {
+        "names": [],
+        "roles": []
+    },
+    "members": {
+        "names": ["reader"],
+        "roles": []
     }
+}
 "@
-        Grant-CouchDBDatabasePermission -Database test -Data $sec -Authorization "admin:password" | Should -Be "true"
+        (Grant-CouchDBDatabasePermission -Database test -Data $sec -Authorization "admin:password").ok | Should -Be "true"
     }
 }
 
@@ -76,7 +76,7 @@ Describe "Get-CouchDBDatabaseSecurity" {
 
 Describe "Revoke-CouchDBDatabasePermission" {
     It "Revoke all permission on database." {
-        Revoke-CouchDBDatabasePermission -Database test -Authorization "admin:password" -Force
+        (Revoke-CouchDBDatabasePermission -Database test -Authorization "admin:password" -Force).ok | Should -Be "true"
     }
 }
 
